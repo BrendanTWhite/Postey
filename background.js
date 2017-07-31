7f68b416b6d9b9f826a7449aefb1589a39422b05
Brendan T White <brendan@thespia.com>
2017-07-31 13:25:16 +0930

    Removed some leftover code from the sample extension I started from

--- background.js
+++ background.js
@@ -10,35 +10,13 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
         const safeUrl = escapeHTML(info.linkUrl);
         const html = `This is HTML: <a href="${safeUrl}">${safeUrl}</a>`;
 
-        // The example will show how data can be copied, but since background
-        // pages cannot directly write to the clipboard, we will run a content
-        // script that copies the actual content.
-
-        // clipboard-helper.js defines function copyToClipboard.
-        const code = "copyToClipboard(" +
-            JSON.stringify(text) + "," +
-            JSON.stringify(html) + ");";
-
         browser.windows.create({
             url: "http://www.thespia.com/" + "Hello"
             //type: "popup",
-        }).then((results) => {
-            // The content script's last expression will be true if the function
-            // has been defined. If this is not the case, then we need to run
-            // clipboard-helper.js to define function copyToClipboard.
-            if (!results || results[0] !== true) {
-                return browser.tabs.executeScript(tab.id, {
-                    file: "clipboard-helper.js",
-                });
-            }
-        }).then(() => {
-            return browser.tabs.executeScript(tab.id, {
-                code,
-            });
         }).catch((error) => {
             // This could happen if the extension is not allowed to run code in
             // the page, for example if the tab is a privileged page.
-            console.error("Failed to copy text: " + error);
+            console.error("Failed to share Facebook link: " + error);
         });
     }
 });
